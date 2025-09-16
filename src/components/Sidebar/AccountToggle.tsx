2632bf5affc36401f5c09fb64ef66cd28ea21e4f
Rabail Waheed <rabailw99@gmail.com>
2025-09-16 10:43:01 +0500

    ABL - ZMS Lofo in Header Size Reduce

--- src/components/Sidebar/AccountToggle.tsx
+++ src/components/Sidebar/AccountToggle.tsx
@@ -6,7 +6,7 @@ const AccountToggle = ({ isCollapsed, activeInterface }: { isCollapsed: boolean;
     <div className="mb-12 mt-4 ml-4 pb-4 border-stone-300 h-6 dark:text-white">
       <img
         src={activeInterface === 'ZMS' ? 'https://res.cloudinary.com/dxqvklctk/image/upload/v1755069630/ZMS-logo_nrn49l.png' : 'https://res.cloudinary.com/dxqvklctk/image/upload/v1757168762/ABL_Logo_1_yiiqgs.png'}
-        className={`h-[10vh] w-27 ${isCollapsed ? 'hidden' : 'block'}`}
+        className={`h-[7vh] w-27 ${isCollapsed ? 'hidden' : 'block'}`}
         alt={activeInterface === 'ZMS' ? 'ZMS Logo' : 'ABL Logo'}
       />
     </div>
