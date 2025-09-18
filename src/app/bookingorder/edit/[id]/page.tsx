752973e7e14f66334f46b11e4c97003eab5567fb
Rabail Waheed <rabailw99@gmail.com>
2025-09-18 15:59:32 +0500

    ABL - BookingOrder edit

--- src/app/bookingorder/edit/[id]/page.tsx
+++ src/app/bookingorder/edit/[id]/page.tsx
@@ -1,13 +1,41 @@
-"use client";
+'use client'
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'next/navigation';
+import { getSingleBookingOrder} from '@/apis/bookingorder';
+import Loader from '@/components/ui/Loader';
 import MainLayout from '@/components/MainLayout/MainLayout'
 import BookingOrderForm from '@/components/ablsoftware/Maintance/BookingOrder.tsx/BookingOrder';
-const ABL = () => {
 
-    return (
-        <MainLayout activeInterface="ABL">
-              <BookingOrderForm isEdit={true} />
-        </MainLayout>
-    )
-}
+const UpdateBookingOrder = () => {
+  const { id } = useParams<{ id: string }>(); 
+  const [initialData, setInitialData] = useState(null);
 
-export default ABL
+  const fetchBookingOrder = async (id: string) => {
+    try {
+      const response = await getSingleBookingOrder(id);
+      console.log(response)
+      setInitialData(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    if (id) {
+      fetchBookingOrder(id);
+    }
+  }, [id]);
+
+  return (
+    <MainLayout activeInterface="ABL">
+      {
+        !initialData ? <Loader /> :
+          <div>
+            <BookingOrderForm isEdit={true} />
+          </div>
+      }
+    </MainLayout>
+  );
+};
+
+export default UpdateBookingOrder;
